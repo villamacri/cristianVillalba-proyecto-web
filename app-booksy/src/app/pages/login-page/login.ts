dde7087ee6d0c70ae88af2c5daa82cab33f42611
Cristian Villalba <villalba.macri24@triana.salesianos.edu>
2026-02-02 14:15:19 +0100

    Login angular v1

--- app-booksy/src/app/pages/login-page/login.ts
+++ app-booksy/src/app/pages/login-page/login.ts
@@ -1,12 +1,55 @@
 import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { Router, RouterLink } from '@angular/router';
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-login',
-  imports: [RouterLink],
+  standalone:true,
+  imports: [FormsModule, RouterLink],
   templateUrl: './login.html',
   styleUrl: './login.css',
 })
 export class Login {
 
+  email: string = '';
+  password: string = '';
+  errorMessage: string = '';
+
+  constructor(private authService: AuthService, private router: Router){}
+
+  onLogin(){
+    this.errorMessage = '';
+
+    const credentials = {
+      email: this.email,
+      password: this.password
+    };
+
+    console.log('Enviando datos a Laravel...', credentials);
+
+    this.authService.login(credentials).subscribe({
+      next:(response: any) => {
+        console.log('Respuesta recibida:', response);
+
+        if(response.token){
+          localStorage.setItem('token', response.token);
+          this.router.navigate(['/dashboard']);
+        }else{
+          this.errorMessage = 'Error: no se recibió el token de seguridad.';
+        }
+      },
+      error: (error)=>{
+        console.error('Error en el login:', error);
+
+        if(error.status === 401){
+          this.errorMessage = 'Correo o contraseña incorrectos.';
+        }else if (error.status === 0){
+          this.errorMessage = 'No se puede conectar con el servidor.'
+        }else{
+          this.errorMessage = 'Ocurrió un error inesperado. Inténtalo más tarde.'
+        }
+      }
+    })
+  }
 }
---
+++ app-booksy/src/app/services/auth.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+@Injectable({
+  providedIn: 'root',
+})
+export class AuthService {
+  private apiUrl = 'http://127.0.0.1:8000/api';
+
+  constructor(private http: HttpClient) {}
+
+  login(credentials: any): Observable<any> {
+    return this.http.post(`${this.apiUrl}/login`, credentials);
+  }
+
+  register(userData: any):Observable<any>{
+    return this.http.post(`${this.apiUrl}/register`, userData);
+  }
+}
